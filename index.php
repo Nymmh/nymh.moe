9df452b113f7a7478e07b27a82cd131291344600
Nymh <stickman7415@gmail.com>
2018-02-03 03:53:28 -0800

    Updated css and php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	require('php/head.php');
+?>
+<body>
+<?php
+	require('php/mainnav.php');
+?>
+<main>
+	<img src="assets/bg/kanna.jpg" alt="kanna a dragon maid" style="max-width:100%;height:auto;margin-top:-202px;position:fixed;overflow:hidden;">
+</main>
+<?php
+	require('php/dropdownscript.php');
+?>
+</body>
+</html>
